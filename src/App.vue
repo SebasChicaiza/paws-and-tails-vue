b4b65ab3d9d5eaeb165faed469ba5ed5e25a6a89
SebasChicaiza <sebastian.erazo275@gmail.com>
2025-06-13 08:27:36 -0500

    Primer commit, pagina web esqueleto

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/images/logoPawsTails-noFondo.png"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav></nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
